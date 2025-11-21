6569d1aa02087fa5a04dee039e7648fcf8be0b30
Isaac de Medeiros <isaac2005medeiros@gmail.com>
2025-11-21 10:17:54 -0300

    realização do Desafio 1

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Hello World!")
+// Armanzenar o Nome do Herói e Experiencia (XP)
+// Estrutura de decisão pra apresentar uma mensagem
+// <= 1000 Ferro; <= 2000 Bronze <= 5000 Prata 
+// <= 7000 Ouro; 8000 <= Platina 
+// <= 9000 Ascendente; <= 10000 Imortal else radiante
+
+let nomeHeroi = "José Roberto"
+let xp = 10001
+let rank
+
+if (xp <= 1000) {
+    rank = "Ferro"
+} 
+else if (xp <= 2000) {
+    rank = "Bronze"
+}
+else if (xp <= 5000) {
+    rank = "Prata"
+}
+else if (xp <= 7000) {
+    rank = "Ouro"
+}
+else if (xp <= 8000) {
+    rank = "Platina"
+}
+else if (xp <= 9000) {
+    rank = "Ascendente"
+}
+else if (xp <= 10000) {
+    rank = "Imortal"
+}
+else {
+    rank = "Radiante"
+}
+console.log("Olá " + nomeHeroi + "!!!")
+console.log("Você tem " + xp + " de XP!")
+console.log("Você está no Rank " + rank + "\n")
